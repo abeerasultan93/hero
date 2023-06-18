7764b2b996b7d8be51b98bd7775d047b190155d3
Abeera Sultan <130004719+abeerasultan93@users.noreply.github.com>
2023-06-18 18:47:29 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+<h1>Woolworths Insurance</h1>
+<!-- wp:paragraph -->
+<p><?php echo "Woolworths Insurance is an insurance service offered by Woolworths Group, one of Australia's leading retail companies. Woolworths Insurance aims to provide a range of insurance products to protect individuals, families, and their assets against various risks and unforeseen events."; ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php echo "Woolworths Insurance offers a diverse portfolio of insurance options, including car insurance, home and contents insurance, travel insurance, pet insurance, life insurance at"; ?> <strong><a href="https://finderhub.com.au/">Finder Hub</a></strong>.<?php echo " These insurance products are designed to cater to the specific needs of customers, offering financial protection and peace of mind in different areas of their lives."; ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php echo "Customers can expect a user-friendly experience when obtaining insurance from Woolworths. The company strives to provide convenient and accessible services, allowing individuals to purchase and manage their insurance policies online or through their customer service channels. With a focus on customer satisfaction, Woolworths Insurance aims to deliver reliable coverage, prompt claims processing, and dedicated customer support."; ?></p>
+<!-- /wp:paragraph -->
+
+
+</body>
+</html>
